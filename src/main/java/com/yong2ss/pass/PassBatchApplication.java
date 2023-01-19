b4ba0f559a118e8b34c4c05eadc6550e04415ef9
jeongyolee <jeongyolee@ebay.com>
2023-01-19 21:35:32 +0900

    batch 기본 test

--- src/main/java/com/yong2ss/pass/PassBatchApplication.java
+++ src/main/java/com/yong2ss/pass/PassBatchApplication.java
@@ -1,10 +1,52 @@
 package com.yong2ss.pass;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+@EnableBatchProcessing
 @SpringBootApplication
 public class PassBatchApplication {
+
+	@Autowired
+	private final JobBuilderFactory jobBuilderFactory;
+
+	@Autowired
+	private final StepBuilderFactory stepBuilderFactory;
+
+	public PassBatchApplication(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
+		this.jobBuilderFactory = jobBuilderFactory;
+		this.stepBuilderFactory = stepBuilderFactory;
+	}
+
+	@Bean
+	public Step passStep() {
+		return this.stepBuilderFactory.get("passStep")
+				.tasklet(new Tasklet() {
+					@Override
+					public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+						System.out.println("Execute PassStep");
+						return RepeatStatus.FINISHED;
+					}
+				}).build();
+	}
+
+	@Bean
+	public Job passJob() {
+		return this.jobBuilderFactory.get("passJob")
+				.start(passStep())
+				.build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PassBatchApplication.class, args);
