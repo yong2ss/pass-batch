3f43009d104b062c7f4589ec56afe5bf06479dd1
jeongyolee <jeongyolee@ebay.com>
2023-01-19 21:25:07 +0900

    first commit

---
+++ src/main/java/com/yong2ss/pass/PassBatchApplication.java
@@ -0,0 +1,13 @@
+package com.yong2ss.pass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassBatchApplication.class, args);
+	}
+
+}
