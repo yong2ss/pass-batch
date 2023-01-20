683f52f61b0e74b0aababedbd0c69a56ba96b5f1
jeongyolee <jeongyolee@ebay.com>
2023-01-20 15:14:24 +0900

    feat : 이용권 만료 Batch itemreader/writer

---
+++ src/main/java/com/yong2ss/pass/job/pass/ExpirePassesJobConfig.java
@@ -0,0 +1,85 @@
+package com.yong2ss.pass.job.pass;
+
+import com.yong2ss.pass.repository.pass.PassEntity;
+import com.yong2ss.pass.repository.pass.PassStatus;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.database.JpaCursorItemReader;
+import org.springframework.batch.item.database.JpaItemWriter;
+import org.springframework.batch.item.database.builder.JpaCursorItemReaderBuilder;
+import org.springframework.batch.item.database.builder.JpaItemWriterBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManagerFactory;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Configuration
+public class ExpirePassesJobConfig {
+    private final int CHUNK_SIZE = 5;
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final EntityManagerFactory entityManagerFactory;
+
+    public ExpirePassesJobConfig(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory, EntityManagerFactory entityManagerFactory) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+        this.entityManagerFactory = entityManagerFactory;
+    }
+
+    @Bean
+    public Job expirePassesJob() {
+        return this.jobBuilderFactory.get("expirePassesJob")
+                .start(expirePassesStep())
+                .build();
+    }
+
+    @Bean
+    public Step expirePassesStep() {
+        return this.stepBuilderFactory.get("expirePassesStep")
+                .<PassEntity, PassEntity>chunk(CHUNK_SIZE)
+                .reader(expirePassesItemReader())
+                .processor(expirePassesItemProcessor())
+                .writer(expirePassesItemWriter())
+                .build();
+    }
+
+    //JpaCursorItemReader Spring 4.3 추가
+    //페이징 보다 높은 성능 및 데이터 변경에 무관한 무결성 조회 가능
+    @Bean
+    @StepScope
+    private JpaCursorItemReader<PassEntity> expirePassesItemReader() {
+        return new JpaCursorItemReaderBuilder<PassEntity>()
+                .name("expirePassesItemReader")
+                .entityManagerFactory(entityManagerFactory)
+                .queryString("select p from PassEntity p where p.status = :status and p.endedAt <= :endedAt")
+                .parameterValues(Map.of("status", PassStatus.IN_PROGRESS, "endedAt", LocalDateTime.now()))
+                .build();
+    }
+
+    @Bean
+    public ItemProcessor<PassEntity, PassEntity> expirePassesItemProcessor() {
+        return passEntity -> {
+            passEntity.setStatus(PassStatus.EXPIRED);
+            passEntity.setExpiredAt(LocalDateTime.now());
+            return passEntity;
+        };
+    }
+
+    // JpaItemWriter : JPA의 영속성 관리를 위해 EntityManager 필수 설정
+    @Bean
+    public JpaItemWriter<PassEntity> expirePassesItemWriter() {
+        return new JpaItemWriterBuilder<PassEntity>()
+                .entityManagerFactory(entityManagerFactory)
+                .build();
+    }
+
+}
+
